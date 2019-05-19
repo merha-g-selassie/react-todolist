1cf5237d71575e98cc0ec34fe1fc5e8987584058
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-05-19 13:50:54 +0200

    Updates

---
+++ precache-manifest.905befd6d97e503dcf85dc65a7f8a18a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ed21eb063da194fd3c5b91d2d7ab56",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "ad7f5269dc2f5cf968be",
+    "url": "/react-todolist/static/css/main.6d5473cc.chunk.css"
+  },
+  {
+    "revision": "60b35a1e1ed30e08969e",
+    "url": "/react-todolist/static/js/2.500deffa.chunk.js"
+  },
+  {
+    "revision": "ad7f5269dc2f5cf968be",
+    "url": "/react-todolist/static/js/main.5bc2801e.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
